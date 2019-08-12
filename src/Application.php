569574899e7884661a61e130c9bf2223a2151335
yonghua4413 <254274509@qq.com>
2019-08-12 17:07:58 +0800

    初始化

---
+++ src/Application.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace yonghua4413\OfficialAccount;
+
+/**
+ * 公众号
+ * Class Application
+ * @package yonghua4413\OfficialAccount
+ */
+
+class Application
+{
+
+}
---
+++ src/Auth/ServiceProvider.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace yonghua4413\OfficialAccount\Auth;
+
+class ServiceProvider
+{
+
+}
